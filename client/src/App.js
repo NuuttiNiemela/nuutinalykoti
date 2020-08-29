6805de494c282fce67ed4b29ed7ab67b1c289315
NuuttiNiemela <nuutti.niemela@hotmail.com>
2020-08-29 15:51:51 +0300

    First buttons for lights

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
+
 const App = props => {
+
     useEffect(() => {
         axios.get('/api/hello')
             .then(res => setState(res.data))
     }, [])
+
+    const light = (what) => {
+        axios.get('/0/' + what)
+            .then(res => setState({light: res}))
+    }
+
     const [state, setState] = useState('')
     return(
         <div>
-            <p>{state.response_text}</p>
+            <button onClick={() => light('0')}>Pois</button>
+            <button onClick={() => light('1')}>Päälle</button>
+            <button onClick={() => light('red')}>Punainen</button>
+            <button onClick={() => light('green')}>Vihreä</button>
+            <button onClick={() => light('blue')}>Sininen</button>
+            <button onClick={() => light('yellow')}>Keltainen</button>
+            <button onClick={() => light('janne')}>Janne</button>
         </div>
     )
 };
